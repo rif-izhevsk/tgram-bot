b949548be9975d7847895072dcfc9220da269dc5
Исламов Рифат <rif.izhevsk@mail.ru>
2018-02-17 20:53:59 +0400

    init commit

---
+++ testbot/src/main/java/izh/tgram/testbot/testbot/TestbotApplication.java
@@ -0,0 +1,12 @@
+package izh.tgram.testbot.testbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbotApplication.class, args);
+	}
+}
